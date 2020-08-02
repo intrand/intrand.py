fb82f5df3f855840a286740e8b33cf02f0948ccb
intrand <intrand@users.noreply.github.com>
2020-08-02 19:25:24 +0000

    support overwriting existing values; breaks API

--- intrand/dict_utils.py
+++ intrand/dict_utils.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 
-def merge(_input={}, _output={}):
+def merge(incoming={}, output={}, overwrite=False):
 	"""
-	Recursively puts missing fields/values into _output (dict) using _input (dict) as input.
-	Useful for, eg, applying some default values to a dictionary of user inputted configuration.
+	Resursively merges two dictionaries by taking inputs from the 'incoming' dictionary and overlaying them upon the 'output' dictionary.
+
+	By default, no values in the resulting dictionary will be overwritten if present in both.
+	Passing 'overwrite=True' will flip this behavior.
+
+	Useful for, eg, applying some user-inputted configuration (incoming) over top of some already-existing (default) values (output).
 	"""
-	_output = _output.copy()
-	for _key, _value in _input.items(): # loop through each key/value pair
+	_output = output.copy()
+	for _key, _value in incoming.items(): # loop through each key/value pair
 		if (_key in _output) and isinstance(_value, dict): # detect when we need to recurse
 			_output[_key] = merge(_value, _output[_key]) # recurse
 		else: # _key is not in output
-			if not _key in _output: # don't overwrite existing values
-				_output[_key] = _value # add missing key/value pair
+			if _key in _output and overwrite == False: # we check if it already exists, and if we care
+				continue # don't overwrite existing values unless overwrite is 'True'
+			_output[_key] = _value # add key/value pair
 
 	return _output # give back the merged dict
 
