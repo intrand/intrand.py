c4e8726e1195d5ffc3f9c327af8978dece4439ac
intrand <intrand@users.noreply.github.com>
2020-08-02 19:28:16 +0000

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="intrand",
-    version="0.0.5",
+    version="0.0.6",
     author="intrand",
     author_email="intrand@intrand.io",
     description="various handy utilities",
